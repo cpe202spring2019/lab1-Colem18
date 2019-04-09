cc48fb6c43ace8b6f64a09679e82e6381d6331a9
Colem18 <cole2018@gmail.com>
2019-04-09 11:03:14 -0700

    done 2

--- lab1.py
+++ lab1.py
@@ -1,4 +1,6 @@
-
+# take in a list of integers. if the input is none, then raise an error. if the list is empty, return none. if the list is filled, iterate through it. while iterating
+# through the list, check each integer against the maximum value variable. if the list number is smaller, continue on with the iteration. But if the list
+# number is bigger, set the max value variable to that number. return the max value variable at the end.
 def max_list_iter(int_list):  # must use iteration not recursion
    if int_list == None:
       raise ValueError
@@ -11,11 +13,17 @@ def max_list_iter(int_list):  # must use iteration not recursion
             maxnum = x
       return maxnum
 
+# take in a list of integers. if the input is none, raise an error. if the input is an empty list, return none. if the inputed list has at least 1 value in it, check the length
+# of the list. if the length of the list is 1, return the list as it is. Otherwise take the value of the first item in the list, run a slice of the list from the
+# second item to the end through this same function, essentially removing the first item. After receiving the returned list from that statement, append what was
+# originally the first value in the list to the end of this new list and return the resulting list
 def reverse_rec(int_list):   # must use recursion
    if int_list == None:
       raise ValueError
+   elif int_list == []:
+      return None
    else:
-      if len(int_list) == 0:
+      if len(int_list) == 1:
          return int_list
       else:
          temp = int_list[0]
@@ -23,6 +31,11 @@ def reverse_rec(int_list):   # must use recursion
          returnList.append(temp)
          return returnList
 
+# take in a target number, a lower bound index, a higher bound index, and a list of integers. if there is no list inputed, raise an error. Otherwise begin looking
+# for the target number. first, create a middle index variable by taking the double division average of the high and low bounds. Then check if the middle is equal
+# to the target number. if so, return the middle index. Otherwise, check if the the target number is smaller than the middle number. if so, call this method with
+# the same parameters except the high is replaced with one index lower than the middle index. return this method call. Otherwise, the target number is deemed higher than the middle, so
+# call this method with all the original parameters except the lower is one index above the medium. return this method call
 def bin_search(target, low, high, int_list):  # must use recursion
    if int_list == None:
       raise ValueError
--- lab1_test_cases.py
+++ lab1_test_cases.py
@@ -5,29 +5,28 @@ from lab1 import *
 class TestLab1(unittest.TestCase):
 
     def test_max_list_iter(self):
-        """add description here"""
         tlist = None
         with self.assertRaises(ValueError):  # used to check for exception
             max_list_iter(tlist)
-        self.assertEqual(max_list_iter([]),None)
-        self.assertEqual(max_list_iter([2,5,1,4,7,3,2,8,0]),8)
+        self.assertEqual(max_list_iter([]),None) # test what the output is when the list is empty
+        self.assertEqual(max_list_iter([2,5,1,4,7,3,2,8,0]),8) # test what the output is when the list is filled with integers
 
     def test_reverse_rec(self):
         tlist = None
-        with self.assertRaises(ValueError):
+        with self.assertRaises(ValueError): # used to check for exception
             reverse_rec(tlist)
-        self.assertEqual(reverse_rec([2,4,6,8,10]),[10,8,6,4,2])
-        self.assertEqual(reverse_rec([1,2,3]),[3,2,1])
+        self.assertEqual(reverse_rec([]),None) # test if the list is empty
+        self.assertEqual(reverse_rec([1,2,3]),[3,2,1]) # test when list is filled with integers
 
     def test_bin_search(self):
-        with self.assertRaises(ValueError):
+        with self.assertRaises(ValueError): # used to check for exception
             bin_search(2,0,7,None)
         list1 = [7,12,16,23,28,30,35,43,49,59,61]
-        self.assertEqual(bin_search(14,0,len(list1)-1,list1),None)
+        self.assertEqual(bin_search(14,0,len(list1)-1,list1),None) # test for when the the list doesn't contain the target number
         list_val =[0,1,2,3,4,7,8,9,10]
         low = 0
         high = len(list_val)-1
-        self.assertEqual(bin_search(4, 0, len(list_val)-1, list_val), 4 )
+        self.assertEqual(bin_search(4, 0, len(list_val)-1, list_val), 4 ) # test for when the list does contain the target number
 
 if __name__ == "__main__":
         unittest.main()
